344898ada9ad4d75000b55351ecb778da47b1a69
MyUsernameDenisTrimignogit config --global user.email my@email.comdenis@trimigno.it <denis@trimigno.it>
2023-05-16 17:43:32 +0200

    fine

---
+++ pag.js
@@ -0,0 +1,11 @@
+// document.getElementById('name').innerHTML = 'ciao'
+
+let name = prompt ('come ti chiami');
+
+let surname = prompt ('Quale è il tuo cognome');
+
+let favouriteColor = prompt ('quale è il tuo colore preferito');
+
+document.getElementById ('resoult').innerHTML = name + surname + favouriteColor + '21'
+
+console.log(`ciao mi chiamo ${name} ${surname}amo il ${favouriteColor} e ho ${21} anni`);
